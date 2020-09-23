008865ef90f7a03fa73fc333f56a609a2fa9b8ae
troye <troye@forfuns.net>
2020-09-23 10:35:36 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import ComColorButton from './packages/comp/src/components/HelloWorld.vue
-import Vue from 'Vue'
-// 註冊組件庫
-Vue.use(ComColorButton)
-createApp(App).mount('#app')
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app')
